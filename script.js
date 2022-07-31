163d1e795a0533e57ac3a67389d1b5264e5bed01
Josh <jatwork321@gmail.com>
2022-07-31 15:20:12 -0400

    health bar, player HP, player2 HP, the timer, and game conditions have been added

--- script.js
+++ script.js
@@ -148,6 +148,37 @@ function hurtboxCollision({ rect1, rect2 }) {
     return true;
   }
 }
+
+function determineWinner({ player, player2, timerID }) {
+  clearTimeout(timerID);
+  document.querySelector("#displayText").style.display = "flex";
+  if (player.health === player2.health) {
+    document.querySelector("#displayText").innerHTML = "Tie";
+  } else if (player.health > player2.health) {
+    document.querySelector("#displayText").innerHTML = "Player 1 Wins";
+  } else if (player2.health > player.health) {
+    document.querySelector("#displayText").innerHTML = "Player 2 Wins";
+  }
+}
+
+// This is for the timer.
+// If player1 has a higher HP, player1 wins and vice versa.
+// If the HP's of both players
+let timer = 10;
+let timerID;
+function decreaseTimer() {
+  timerID = setTimeout(decreaseTimer, 1000);
+  if (timer > 0) {
+    timer--;
+    document.querySelector("#timer").innerHTML = timer;
+  }
+  if (timer === 0) {
+    determineWinner({ player, player2, timerID });
+  }
+}
+
+decreaseTimer();
+
 function animate() {
   window.requestAnimationFrame(animate);
   context.fillStyle = "black";
@@ -199,6 +230,11 @@ function animate() {
     player2.isAttacking = false;
     player.health -= 20;
     document.querySelector("#playerHP").style.width = player.health + "%";
+  }
+
+  // If the HP of either player reaches zero, end the game
+  if (player.health <= 0 || player2.health <= 0) {
+    determineWinner({ player, player2, timerID });
   }
 }
 
